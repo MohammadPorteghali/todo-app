b9673bcd5375a98b04839a471763b7c30bb5d948
Mohammad Porteghali <mohammadmp7@gmail.com>
2021-04-24 15:06:52 +0300

    new todo debugged

--- src/components/Landing.vue
+++ src/components/Landing.vue
@@ -34,7 +34,7 @@
             <span v-show="!item.done" class="fill-button"></span>
           </button>
           {{ item.title }}
-          <button class="cross" @click="removeItem(index)">
+          <button class="cross" @click.stop="removeItem(index)">
             <svg xmlns="http://www.w3.org/2000/svg" width="18" height="18"><path fill="#494C6B" fill-rule="evenodd" d="M16.97 0l.708.707L9.546 8.84l8.132 8.132-.707.707-8.132-8.132-8.132 8.132L0 16.97l8.132-8.132L0 .707.707 0 8.84 8.132 16.971 0z"/></svg>
           </button>
         </div>
@@ -80,15 +80,13 @@ export default {
       this.dark = !this.dark
     },
     createTodo() {
-      this.todoList.push({
-        title: this.newTodo,
-        done: false
-      })
-      this.activeList.push({
-        title: this.newTodo,
-        done: false
-      })
-      this.newTodo = '';
+      if (this.newTodo) {
+        this.todoList.push({
+          title: this.newTodo,
+          done: false
+        })
+        this.newTodo = '';
+      }
     },
     removeItem(index) {
       this.todoList.splice(index, 1)
